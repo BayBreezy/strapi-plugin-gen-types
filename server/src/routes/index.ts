e22c9dd50062b61852b6466679935a795a10d34f
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-02-18 05:33:23 -0500

    refactor: restructure route configuration to include admin type and improve organization
    Since this is a dev only endpoint and it really is only needed in the admin part of things, I think making it admin only is the right thing to do

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,13 @@
-export default [
-  {
-    method: "GET",
-    path: "/",
-    handler: "controller.index",
-    config: {
-      policies: [],
-    },
+export default {
+  admin: {
+    type: "admin",
+    routes: [
+      {
+        method: "GET",
+        path: "/",
+        handler: "controller.index",
+        config: { auth: false, policies: [] },
+      },
+    ],
   },
-];
+};
