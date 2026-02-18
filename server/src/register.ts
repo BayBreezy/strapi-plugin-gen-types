d87a514a20416bf91086b8eb90e6e07cf53f73c9
Behon Baker <53905713+BayBreezy@users.noreply.github.com>
2026-02-17 20:17:44 -0500

    refactor: reorder log statements for improved clarity in plugin registration

--- server/src/register.ts
+++ server/src/register.ts
@@ -3,14 +3,14 @@ import type { GenTypesConfig } from "./config";
 import { pluginName } from "./genTypes/types";
 
 const register = ({ strapi }: { strapi: Core.Strapi }) => {
-  strapi.log.info("Gen Types Plugin registered");
   if (process.env.NODE_ENV === "production") {
     strapi.log.info("Gen Types Plugin is disabled in production");
   } else {
     const config: GenTypesConfig = strapi.config.get("plugin::gen-types");
     strapi
-      .service(`plugin::${pluginName}.service`)
-      .generateInterfaces(config.outputLocation, config.singleFile, config?.prettier?.singleQuote);
+    .service(`plugin::${pluginName}.service`)
+    .generateInterfaces(config.outputLocation, config.singleFile, config?.prettier?.singleQuote);
+    strapi.log.info("Gen Types Plugin registered");
   }
 };
 
